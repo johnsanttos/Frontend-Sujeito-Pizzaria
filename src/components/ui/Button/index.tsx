e97b0689ede77a2a16d1b3a52b1e852bb9f7c496
John <jhow.mito11@gmail.com>
2022-08-25 10:46:54 -0300

    button component

---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, ButtonHTMLAttributes } from 'react'
+import styles from './styles.module.scss'
+
+interface ButtonProps extends ButtonHTMLAttributes <HTMLButtonElement> {
+    loading?: boolean,
+    children: ReactNode,
+}
+
+export function Button ({loading, children, ...rest}: ButtonProps) {
+    return (
+        <button className = {styles.button}
+        disabled ={loading}
+        {...rest}
+        >
+          
+        <a className = {styles.buttonText}
+        >{children}</a>
+        </button>
+    )
+}
