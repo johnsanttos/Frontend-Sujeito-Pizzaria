eff2e918c8b236d99c385ff4f568831e1310661c
John Santos <84046012+johnsanttos@users.noreply.github.com>
2022-11-03 15:51:27 -0300

    Login finalizado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,59 +1,85 @@
-import { destroyCookie } from "nookies";
-import Router from "next/router";
-import { createContext, ReactNode, useState } from "react"
+import { destroyCookie, setCookie, parseCookies } from 'nookies'
+import { api } from '../services/apiClient'
+import Router from 'next/router'
+import { createContext, ReactNode, useState } from 'react'
 
 type AuthContextData = {
-    user: UserProps
-    isAutheticated: boolean
-    signIn: (credentials: SignInProps) => Promise<void>
-    signOut: () => void
+  user: UserProps
+  isAutheticated: boolean
+  signIn: (credentials: SignInProps) => Promise<void>
+  signOut: () => void
 }
 
 type UserProps = {
-    id: string
-    name: string
-    email: string
+  id: string
+  name: string
+  email: string
 }
 
 type SignInProps = {
-    email: string
-    password: string
+  email: string
+  password: string
 }
 
 type AuthProviderProps = {
-    children: ReactNode
+  children: ReactNode
 }
 //Children é nossas paginas que esta dentro do contexto
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function signOut(){
-    try{
-        destroyCookie(undefined,'@nextauth.token')
-        //destroyCookie vai destruir o token usuario salvo em '@nextauth.token'
-        Router.push('/')
-    } catch{
-        console.log('erro ao deslogar')
-    }
+export function signOut() {
+  try {
+    destroyCookie(undefined, '@nextauth.token')
+    //destroyCookie vai destruir o token usuario salvo em '@nextauth.token'
+    Router.push('/')
+  } catch {
+    console.log('erro ao deslogar')
+  }
 }
 
-
 export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>()
+  //!!user duas esclamações antes da variavel significa que esta convertendo em booleano, se user estiver vazio vai converter para falso, se tiver algo na varialvel sera true, estara logado
+  const isAutheticated = !!user
 
-    const [user,setUser] = useState <UserProps>()
-    //!!user duas esclamações antes da variavel significa que esta convertendo em booleano, se user estiver vazio vai converter para falso, se tiver algo na varialvel sera true, estara logado
-    const isAutheticated = !!user
+  async function signIn({ email, password }: SignInProps) {
+    try {
+      const response = await api.post('/session', {
+        email,
+        password,
+      })
 
-   async function signIn({email,password}: SignInProps) {
-      alert('Clicou karaiiii')
-    console.log( 'Dados email ', email)
-    console.log( 'Dados password ', password)
+      //descontruindo response.data
+      const { id, name, token } = response.data
+
+      // console.log('ariosvaldo ', response.data)
+
+      //salvando o token no cookies
+      setCookie(undefined, '@nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, //expirar em 1 mes
+        path: '/', // quais caminhos terao acesso ao cookies "/" significa todes
+      })
+
+      setUser({
+        id,
+        name,
+        email,
+      })
+      // passar para as proximas resições o nosso token
+      api.defaults.headers['authorizathion'] = `Bearer ${token}`
+
+      //redirecionar para a /dashboard
+      Router.push('/dashboard')
+      
+    } catch (err) {
+      console.log('ERRO AO ACESSAR ', err)
     }
+  }
 
-
-    return (
-        <AuthContext.Provider value={{user,isAutheticated, signIn , signOut}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ user, isAutheticated, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard (){
+	return (
+		<div>
+			<h1> Bem vindo ao dashboard</h1>
+		</div>
+	)
+}
