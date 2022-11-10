0153f6a1d2edba4e9be9951daa723ae733721e3f
John Santos <84046012+johnsanttos@users.noreply.github.com>
2022-11-10 17:02:28 -0300

    Componente preview

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,9 +1,33 @@
+import { ChangeEvent, useState } from 'react'
 import styles from './styles.module.scss'
 import Head from 'next/head'
 import { Header } from '../../components/Header'
+import { FiUpload } from 'react-icons/fi'
 import { canSSRAuth } from '../../utils/canSSRAuth'
 
-export default function Pruduct() {
+export default function Product() {
+  const [avatarUrl, setAvatarUrl] = useState('')
+  const [imageVatar, setImageAvatar] = useState(null)
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) {
+      return
+    }
+
+    const image = e.target.files[0]
+    if (!image) {
+      return
+    }
+
+    if(image.type === 'image/jpeg' || image.type === 'image/png') {
+
+      setImageAvatar(image)
+      //  URL.createObjectURL cria um html do objeto para o  preview ficticia para exibir image antes de mandar para o backend
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+
+    }
+  }
+
   return (
     <>
       <Head>
@@ -14,38 +38,58 @@ export default function Pruduct() {
         <Header />
 
         <main className={styles.container}>
+          
           <h1>Novo Produto</h1>
 
-
-
-
           <form className={styles.form}>
+
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#fff" />
+              </span>
+
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+
+              {avatarUrl && (
+                <img
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt=""
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+
             <select>
               <option>Bebida</option>
               <option>pizza</option>
             </select>
 
             <input
-            type= "text"
-            placeholder='Digite o nome do produto'
-            className={styles.input}
+              type="text"
+              placeholder="Digite o nome do produto"
+              className={styles.input}
             />
 
             <input
-            type= "text"
-            placeholder='Preço do produto'
-            className={styles.input}
+              type="text"
+              placeholder="Preço do produto"
+              className={styles.input}
             />
-            
+
             <textarea
-            placeholder='Descreva seu produto...'
-            className={styles.input}
+              placeholder="Descreva seu produto..."
+              className={styles.input}
             />
 
             <button className={styles.buttonAdd} type="submit">
               Cadastrar
             </button>
-
           </form>
         </main>
       </div>
