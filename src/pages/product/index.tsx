00fc04fe1141615f05356d779ad96cdd9dc0e24e
John Santos <84046012+johnsanttos@users.noreply.github.com>
2022-11-14 17:05:30 -0300

    Cadastrando produto

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,14 +1,14 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import styles from './styles.module.scss'
 import Head from 'next/head'
+import { toast } from 'react-toastify'
 import { Header } from '../../components/Header'
 import { FiUpload } from 'react-icons/fi'
 import { setupAPIClient } from '../../services/api'
 import { canSSRAuth } from '../../utils/canSSRAuth'
-import { type } from 'os'
 
-type ItemProps ={
-  id: string,
+type ItemProps = {
+  id: string
   name: string
 }
 
@@ -17,17 +17,16 @@ interface CategoryProps {
   categoryList: ItemProps[]
 }
 
-export default function Product({categoryList}:CategoryProps) {
-
-  console.log(categoryList)
+export default function Product({ categoryList }: CategoryProps) {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
 
   const [avatarUrl, setAvatarUrl] = useState('')
   const [imageVatar, setImageAvatar] = useState(null)
 
-  const[categories, setCategories] = useState (categoryList || [])
-
-  const[categorySelected, setCategorySelected] = useState(0)
-
+  const [categories, setCategories] = useState(categoryList || [])
+  const [categorySelected, setCategorySelected] = useState(0)
 
   function handleFile(e: ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) {
@@ -39,23 +38,56 @@ export default function Product({categoryList}:CategoryProps) {
       return
     }
 
-    if(image.type === 'image/jpeg' || image.type === 'image/png') {
-
+    if (image.type === 'image/jpeg' || image.type === 'image/png') {
       setImageAvatar(image)
       //  URL.createObjectURL cria um html do objeto para o  preview ficticia para exibir image antes de mandar para o backend
       setAvatarUrl(URL.createObjectURL(e.target.files[0]))
-
     }
   }
   //quando voce seleciona uma nova categoria na lista
-  function handleChangeCategory(event){
+  function handleChangeCategory(event) {
     //console.log ("POSIÇÃO DA CATEGORIA SELECIONADA" ,   event.target.value)
     //console.log ("Categoria selecionada" ,   categories[event.target.value])
 
     setCategorySelected(event.target.value)
-
   }
 
+  async function handleRegister(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+
+      const data = new FormData()
+
+      if(name === "" || price === "" || description === "" || imageVatar === null ){
+        toast.error("Preencha todos os campos!")
+        return
+      }
+
+      //append adicionar o item para requisição
+      data.append('name' , name)
+      data.append('price', price)
+      data.append('description', description)
+      data.append('category_id', categories[categorySelected].id )
+      data.append ('file' , imageVatar)
+
+      const apiClient = setupAPIClient();
+
+      await apiClient.post('/product' , data )
+
+      toast.success('Produto cadastrado com sucesso!')
+
+    } catch (err) {
+      console.log(err)
+      toast.error('Ops erro ao cadastrar!')
+    }
+
+    setName('')
+    setPrice('')
+    setDescription('')
+    setImageAvatar(null)
+    setAvatarUrl('')
+  }
 
   return (
     <>
@@ -67,11 +99,9 @@ export default function Product({categoryList}:CategoryProps) {
         <Header />
 
         <main className={styles.container}>
-          
           <h1>Novo Produto</h1>
 
-          <form className={styles.form}>
-
+          <form onSubmit={handleRegister} className={styles.form}>
             <label className={styles.labelAvatar}>
               <span>
                 <FiUpload size={30} color="#fff" />
@@ -94,39 +124,37 @@ export default function Product({categoryList}:CategoryProps) {
               )}
             </label>
 
-            <select 
-            value={categorySelected}
-            onChange={handleChangeCategory}
-            >
-              {
-                categories.map( (item, index)=>{
-                return(
+            <select value={categorySelected} onChange={handleChangeCategory}>
+              {categories.map((item, index) => {
+                return (
                   <option key={item.id} value={index}>
                     {item.name}
                   </option>
                 )
-                }
-
-                )
-              }
-             
+              })}
             </select>
 
             <input
               type="text"
               placeholder="Digite o nome do produto"
               className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
 
             <input
               type="text"
               placeholder="Preço do produto"
               className={styles.input}
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
             />
 
             <textarea
               placeholder="Descreva seu produto..."
               className={styles.input}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
 
             <button className={styles.buttonAdd} type="submit">
@@ -140,15 +168,14 @@ export default function Product({categoryList}:CategoryProps) {
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-const apiClient = setupAPIClient(ctx)
+  const apiClient = setupAPIClient(ctx)
 
-const response = await apiClient.get('/category')
-//console.log(response.data)
+  const response = await apiClient.get('/category')
+  //console.log(response.data)
 
   return {
-    
     props: {
-      categoryList: response.data
+      categoryList: response.data,
     },
   }
 })
